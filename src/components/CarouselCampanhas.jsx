27277934e70c7d6e11350af15d582c94ec420e10
RaphaelVictor203 <raphael.victor204@gmail.com>
2021-10-25 09:24:34 -0300

    Estilização Carousel de Campanhas

--- src/components/CarouselCampanhas.jsx
+++ src/components/CarouselCampanhas.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 import arrow_right from '../images/arrow_right.png';
 import arrow_left from '../images/arrow_left.png';
+import exemplo_imagem_background from '../images/doacao_exemplo.png';
 
 import '../css/CarouselCampanhas.css';
 
@@ -17,19 +18,19 @@ class CarouselCampanhas extends Component {
                             <img src={arrow_left} className="arrows"/>
                         </div>
                         <div id="item-content">
-                            <Container fluid="md">
+                            <Container fluid="md" id="item-container">
                                 <Row id="title-row">
                                     <p id="titulo-carousel">Organização amigos do bem</p>
                                 </Row>
                                 <Row id="content-row">
-                                    <Col style={{backgroundColor : "red"}}>
+                                    <Col>
                                         <p id="nome-campanha">Campanha do agasalho</p>
                                         <p id="desc-campanha">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                         Aenean pellentesque dolor ante, at convallis turpis euismod a. Sed finibus nisl 
                                         eros, vitae cursus nunc pellentesque a. Integer tempor turpis et dui feugiat, 
                                         vitae gravida nisi maximus. Integer mollis finibus condimentum...</p>
                                     </Col>
-                                    <Col style={{backgroundColor : "black"}}>
+                                    <Col>
                                         <input type="button" value="Quero saber mais" className="btn-acessar"/>
                                     </Col>
                                 </Row>
@@ -43,6 +44,10 @@ class CarouselCampanhas extends Component {
                                     </div>
                                 </Row>
                             </Container>
+                            <div id="item-image-background">
+                                <div id="item-img-effect"></div>
+                                <img src={exemplo_imagem_background} id="item-img"/>
+                            </div>
                         </div>
                         <div className="div-controls">
                             <img src={arrow_right} className="arrows"/>
